2c2ccf7c95df6d5f5cfe8e7f0ea91b12d8024326
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-02 14:42:04 +0800

    semi-responsiveness changes

--- data/information.js
+++ data/information.js
@@ -9,6 +9,7 @@ const info = {
     address: ' Binagonan Rizal, Philippines 1940',
     title: 'Frontend Developer',
     motto: 'insert motto',
+    aboutme: "I'm a graduating Information Technology student at the Polytechnic University of the Philippines, passionate about game development, UI/UX design, and frontend development. I enjoy creating interactive, user-focused experiences through code and design.",
 };
 
 const links = {
