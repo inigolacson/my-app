e794f028dea13869f6f121c3430901d4ef6ec552
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-03 18:08:56 +0800

    added resume download link

--- data/information.js
+++ data/information.js
@@ -18,7 +18,7 @@ const links = {
     gmail: 'https://mail.google.com/mail/?view=cm&fs=1&to=inigolacson01@gmail.com&su=Hello%20from%20your%20portfolio&body=I%20saw%20your%20site%20and%20wanted%20to%20reach%20out!',
     facebook : 'https://www.facebook.com/InigoLacsonT/',
     instagram: 'https://www.instagram.com/inigolacson/',
-    resume: '',
+    resume: 'https://drive.google.com/uc?export=download&id=1d492yrn5a45J_UCBhW08N1pbuStCQKp9',
 }
 
 export default info;
