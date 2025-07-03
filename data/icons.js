2769eef9cc0242f49dfc78f4889614c0f90b94c9
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-03 17:55:49 +0800

    icon imports

--- data/icons.js
+++ data/icons.js
@@ -4,10 +4,62 @@ import {
     FaFacebookSquare, 
     FaInstagram, 
     FaEnvelope, 
-    FaFileAlt 
+    FaFileAlt,
+    FaTeams
     } from 'react-icons/fa';
-    
-import { HiLocationMarker } from 'react-icons/hi';
-import { siJira } from 'react-icons/si'
 
-export { FaGithub, FaLinkedin, FaFacebookSquare, FaInstagram, FaEnvelope, FaFileAlt, HiLocationMarker, siJira }
+import { 
+    HiLocationMarker 
+    } from 'react-icons/hi';
+
+import { 
+    SiReact,
+    SiFlutter,
+    SiPython,
+    SiJavascript,
+    SiTailwindcss,
+    SiSass,
+    SiMysql,
+    SiMssqlserver,
+    SiGit,
+    SiGithub,
+    SiJira,
+    SiMSTeam,
+    SiAdobexd,
+    SiFigma,
+    SiCanva,
+    SiAdobephotoshop,
+    SiAdobeillustrator
+    
+    } from 'react-icons/si'
+
+export { 
+    FaGithub, 
+    FaLinkedin, 
+    FaFacebookSquare, 
+    FaInstagram, 
+    FaEnvelope, 
+    FaFileAlt, 
+    HiLocationMarker, 
+    SiReact,
+    SiFlutter,
+    SiPython,
+    SiJavascript,
+    SiTailwindcss,
+    SiSass,
+    SiMysql,
+    SiMssqlserver,
+    SiGit,
+    SiGithub,
+    SiJira,
+    SiMicrosoftteams,
+    SiAdobexd,
+    SiFigma,
+    SiCanva,
+    SiAdobephotoshop,
+    SiAdobeillustrator
+     }
+
+     //miro
+     //mssqlserver
+     //
