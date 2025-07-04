4c1dc746f39aba28be5d4977bc650ada2e6eafbb
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-07-04 09:08:28 +0800

    added import

--- component/experience/experiencesComponent.js
+++ component/experience/experiencesComponent.js
@@ -1,8 +1,9 @@
+import experience from '@styles/modules/right/experiences.module.scss';
 
 export function ExperienceComponent() {
     return (
         <section>
-            
+
         </section>
     )
 }
--- component/project/projectsComponent.js
+++ component/project/projectsComponent.js
@@ -1,8 +1,9 @@
+import projects from '@styles/modules/right/projects.module.scss';
 
 export function ProjectComponent() {
     return (
         <section>
-            
+
         </section>
     )
 }
